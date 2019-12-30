533e47105b12010bcb68e0718b0bb2aaf428f203
Surendra Nath <surendra.nath@phm.co.uk>
2019-12-30 09:33:58 +0000

    Add Factorial Challenge Code

---
+++ src/IQKata.Domain/FactorialCalculator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Text;
+using System.Threading;
+
+namespace Kata.Domain
+{
+    public static class FactorialCalculator
+    {
+
+        public static string Factorial(int n)
+        {
+            if(n<=0) 
+                return "1";
+            var result =   factorialRecursion(n);
+            return String.Format("{0:F0}", result);
+        }
+  
+        public static double factorialRecursion(int number)
+        {
+            if (number == 1)
+                return 1;
+            return number * factorialRecursion(number - 1);
+        }
+
+    }
+}
